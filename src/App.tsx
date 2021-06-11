f083d66c0e1d160fbd84cda427f1c1a3ae9783ec
nvan23 <dtnvan230898@gmail.com>
2021-06-11 09:19:41 +0700

    change prettier style

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import "./App.css";
+import './App.css'
 
-import { ReactElement } from "react";
+import { ReactElement } from 'react'
 
-import logo from "./logo.svg";
+import logo from './logo.svg'
 
 export default function App(): ReactElement {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	)
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-import "./index.css";
+import './index.css'
 
-import { StrictMode } from "react";
-import { render } from "react-dom";
+import { ConfigProvider } from 'antd'
+import vi_VN from 'antd/lib/locale/vi_VN'
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
 
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
 render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root"),
-);
+	<ConfigProvider locale={vi_VN}>
+		<StrictMode>
+			<App />
+		</StrictMode>
+	</ConfigProvider>,
+	document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
