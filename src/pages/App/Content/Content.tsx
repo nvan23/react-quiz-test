725321e29966651353c1e67c7491b37a3fa41879
nvan23 <dtnvan230898@gmail.com>
2021-06-13 22:32:17 +0700

    add condition to route

--- src/pages/App/Content/Content.tsx
+++ src/pages/App/Content/Content.tsx
@@ -1,7 +1,7 @@
 import './content.css'
 
 import React, { ReactElement } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import HomePage from '../../Home'
 import LoginPage from '../../Login'
@@ -10,11 +10,15 @@ import NotAuthenticated from '../../NotAuthenticated'
 import QuizChallenge from '../../QuizChallenge'
 import ViewResult from '../../ViewResult'
 
+import { isLoggedIn } from '../../../services/user'
+
 export default function Content (): ReactElement {
   return (
     <div className='content__container'>
       <Switch>
-        <Route exact path='/login' component={LoginPage} />
+        <Route exact path='/login'>
+          {isLoggedIn() ? <Redirect to="/" /> : <LoginPage />}
+        </Route>
         <Route exact path='/quiz-challenge' component={QuizChallenge} />
         <Route exact path='/view-result' component={ViewResult} />
         <Route exact path='/' component={HomePage} />
