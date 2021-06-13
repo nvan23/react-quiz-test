8b3da9ee5ac19a99ce50fa86d5026e32b5422603
nvan23 <dtnvan230898@gmail.com>
2021-06-13 22:29:16 +0700

    create login and logout services

---
+++ src/services/user.tsx
@@ -0,0 +1,44 @@
+import {
+  getUser,
+  setUser,
+  removeUser,
+} from '../utils/localStorage'
+
+import { config } from '../config'
+
+export {
+  isLoggedIn,
+  loadUser,
+  login,
+  logout,
+}
+
+function isLoggedIn () {
+  if (!getUser()) return false
+  return true
+}
+
+function loadUser () {
+  return getUser()
+}
+
+function login (username: string, password: string, remember: boolean) {
+  if (username === config.USER.USERNAME && password === config.USER.PASSWORD) {
+    if (remember) {
+      const user: object = {
+        username,
+        password
+      }
+      setUser(user)
+      return true
+    }
+  }
+  return false
+}
+
+function logout () {
+  if (!getUser()) return false
+
+  removeUser()
+  return true
+}
