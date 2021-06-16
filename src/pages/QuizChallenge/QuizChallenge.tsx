2c0be138050424ff73a04fb5011e0b84410b6892
nvan23 <dtnvan230898@gmail.com>
2021-06-16 09:04:12 +0700

    add spin

--- src/pages/QuizChallenge/QuizChallenge.tsx
+++ src/pages/QuizChallenge/QuizChallenge.tsx
@@ -1,34 +1,34 @@
 import { useState, useEffect, ReactElement } from 'react'
-import { AxiosResponse } from 'axios'
 
-import { Typography, Space, Tabs, Alert, Radio, Empty } from 'antd'
+import {
+  Typography,
+  Space,
+  Tabs,
+  Alert,
+  Radio,
+  Empty,
+  Spin,
+} from 'antd'
+
 import Box from '../../components/Box'
 
 import './quiz-challenge.css'
 
 import { getQuiz } from '../../requester/quiz'
 
-interface Quiz {
-  id: number,
-  question: string,
-  choices: {
-    A: string,
-    B: string,
-    C: string,
-    D: string,
-  }
-}
-
-export default function NotFound (): ReactElement {
+export default function QuizChallenge (): ReactElement {
 
   const [visible, setVisible] = useState<boolean>(true)
   const [selectedOption, setSelectedOption] = useState<number>(1)
+  const [questionIndex, setQuestionIndex] = useState<string>('1')
   const [quiz, setQuiz] = useState<Array<object>>([])
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
     getQuiz()
       .then(res => {
         if (res.status) {
+          setLoading(false)
           return setQuiz(res.data.result)
         }
       })
@@ -41,7 +41,7 @@ export default function NotFound (): ReactElement {
   const { TabPane } = Tabs
 
   function callback (key) {
-    console.log(key)
+    setQuestionIndex(key)
   }
 
   function handleClose () {
@@ -56,70 +56,82 @@ export default function NotFound (): ReactElement {
   return (
     <div className='quiz-challenge__container'>
       {
-        quiz?.length
+        loading
           ? (
-            <Space direction="vertical" style={{ width: '100%' }}>
-              <Text strong>Question list for React challenger: 1/3</Text>
-
-              {
-                visible && (
-                  <Alert
-                    message="Choose the best answer of the following question"
-                    type='info'
-                    style={{
-                      width: '100%',
-                    }}
-                    closable
-                    showIcon
-                    afterClose={handleClose} />
-                )
-              }
-
-              <Tabs defaultActiveKey="1" onChange={callback}>
-                {
-                  quiz.map((q: any) => {
-                    return (
-                      <>
-                        <TabPane tab={`Q${q?.id}`} key={q?.id}>
-                          <Space direction="vertical" style={{ width: '100%' }}>
-                            <Box>
-                              <Text strong>{q?.question}</Text>
-                            </Box>
-
-
-
-                            <Radio.Group
-                              onChange={onChange}
-                              value={null}
-                            >
-                              <Space direction="vertical">
-                                {
-                                  Object.entries(q?.choices).map(
-                                    option => {
-                                      return <Radio value={option[0]}>{option[1]}</Radio>
-                                    }
-                                  )
-                                }
-                              </Space>
-                            </Radio.Group>
-                          </Space>
-                        </TabPane>
-                      </>
-                    )
-                  })
-                }
-              </Tabs>
-            </Space>
-
-          )
-          : (
             <div style={{
               display: 'flex',
               margin: 'auto',
               alignItems: 'center',
             }}>
-              <Empty />
+              <Spin size="large" tip="Loading..."></Spin>
             </div>
+          )
+          : (
+            <>
+              {
+                quiz?.length
+                  ? (
+                    <Space direction="vertical" style={{ width: '100%' }}>
+                      <Text key={questionIndex} strong>Question list for React challenger: {questionIndex}/{quiz?.length}</Text>
+
+                      {
+                        visible && (
+                          <Alert
+                            message="Choose the best answer of the following question"
+                            type='info'
+                            style={{
+                              width: '100%',
+                            }}
+                            closable
+                            showIcon
+                            afterClose={handleClose} />
+                        )
+                      }
+
+                      <Tabs defaultActiveKey="1" onChange={callback}>
+                        {
+                          quiz.map((q: any) => {
+                            return (
+                              <TabPane tab={`Q${q?.id}`} key={q?.id}>
+                                <Space direction="vertical" style={{ width: '100%' }}>
+                                  <Box>
+                                    <Text strong>{q?.question}</Text>
+                                  </Box>
+
+                                  <Radio.Group
+                                    onChange={onChange}
+                                    value={null}
+                                  >
+                                    <Space direction="vertical">
+                                      {
+                                        Object.entries(q?.choices).map(
+                                          option => {
+                                            return <Radio key={option[0]} value={option[0]}>{option[1]}</Radio>
+                                          }
+                                        )
+                                      }
+                                    </Space>
+                                  </Radio.Group>
+                                </Space>
+                              </TabPane>
+                            )
+                          })
+                        }
+                      </Tabs>
+
+                    </Space>
+                  )
+                  : (
+                    <div style={{
+                      display: 'flex',
+                      margin: 'auto',
+                      alignItems: 'center',
+                    }}>
+                      <Empty />
+                    </div>
+                  )
+              }
+            </>
           )
       }
     </div >
