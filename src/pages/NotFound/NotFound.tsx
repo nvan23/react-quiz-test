8143d807cda3b2b022dca97f71005c3861b0efcc
nvan23 <dtnvan230898@gmail.com>
2021-06-16 09:03:48 +0700

    add key

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -8,7 +8,7 @@ export default function NotFound (): ReactElement {
   return (
     <div className='not-found__container'>
       <Result
-        key='not-found'
+        key='not-found-result'
         status='404'
         title='404'
         subTitle='Sorry, you have accessed a not-existing route.'
