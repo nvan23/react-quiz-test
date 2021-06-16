35c873b885b4305e13dc9be71228303e770ced02
nvan23 <dtnvan230898@gmail.com>
2021-06-16 08:32:16 +0700

    add key for component

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -8,11 +8,12 @@ export default function NotFound (): ReactElement {
   return (
     <div className='not-found__container'>
       <Result
+        key='not-found'
         status='404'
         title='404'
         subTitle='Sorry, you have accessed a not-existing route.'
         extra={
-          <Button type='primary' href='/'>
+          <Button key='btn-back-home' type='primary' href='/'>
             Back Home
           </Button>
         }
